be9910c0b189f62c418c1566cb2cf09187d0d737
TBSDrJ <marcus.jaiclin@bishops.com>
2022-04-19 09:14:03 -0700

    Write a little more complicated HttpResponse, including a button and GET data.

--- testapp/views.py
+++ testapp/views.py
@@ -5,8 +5,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return HttpResponse(f"<h1>Yo.</h1>")
+        response = HttpResponse(f"<h1>Yo. Method: Get.</h1>\n")
+        response.write(f"<form action='/Jonas'>\n")
+        response.write(f"<input type='text' name='dude' value='Jonas' />\n")
+        response.write(f"<input type='submit' name='submit' value='Submit' />\n")
+        response.write(f"</form>\n")
+        return response
 
 class DudeView(View):
     def get(self, request, dude):
-        return HttpResponse(f"<h1>Dude.</h1>")
+        return HttpResponse(f"<h1>Dude: {dude} Method: Get.</h1>")
+    def get(self, request, dude):
+        return HttpResponse(f"<h1>Dude: {dude} Method: Post.</h1>")
